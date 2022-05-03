1cb53bc328b6b471a1fc49b6ea20a4b6424bc3f0
Vijendra <docs.vijendra@gmail.com>
2022-05-03 10:08:50 +0530

    developed route to post a POST on server

---
+++ src/controllers/createPost.controller.js
@@ -0,0 +1,39 @@
+const express = require('express')
+const Post = require('../models/post.modal')
+const { cloudinary } = require('../utils/cloudinary')
+const router = express.Router()
+
+router.post('/', async ( req, res)=>{
+    try {
+        let body = req.body
+        const image = req.body.image 
+        if(image){
+            const uploadedResponse = await cloudinary
+            .uploader
+            .upload(image, {
+                upload_preset : 'twitter'
+            })
+            const base = `https://res.cloudinary.com/xyzabilliondollarsgoal-gmail-com/image/upload/v`
+            const { version,public_id } = uploadedResponse
+            body = {
+                ...req.body, image : base+version+"/"+public_id
+            }
+        }
+        const post = await Post.create(body)
+        console.log({createPost : post})
+        return res
+        .status(201)
+        .send({ success : "success"})
+    } catch (error) {
+        console.log(error.message)
+        return res
+        .status(500)
+        .send({
+            message : error.message,
+            location : "post-controller"
+        })
+    }
+})
+
+
+module.exports = router
