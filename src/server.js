2f0f6fcc8233a7c3db5f37e31f80de9d1cb79fe0
Vijendra <docs.vijendra@gmail.com>
2022-05-03 10:15:40 +0530

    added search controller path

--- src/server.js
+++ src/server.js
@@ -5,26 +5,31 @@ const mongoConnector = require('./configs/database')
 
 // const flatController = require('./controller/flat.controller')
 // const { register, login } = require('./controller/user.controller')
-const userController = require('./controllers/user.controller')
+// const userController = require('./controllers/user.controller')
 const profileController = require('./controllers/profile.controller')
 const followController = require('./controllers/follow.controller')
-const postController = require('./controllers/post.controller')
+const createPostController = require('./controllers/createPost.controller')
+const getPostsController = require('./controllers/getPosts.controller')
 const followingPostController = require('./controllers/followingpost.controller')
+const googleSignIncontroller = require('./controllers/googleLogin.controller')
+const searchController = require('./controllers/search.controller')
+const { signin, signup } = require('./controllers/account.controller')
 
 const app = express()
-app.use(express.json())
+app.use(express.json({limit : '50mb'}))
+app.use(express.urlencoded({ limit : '50mb', extended : true}))
 app.use(cors())
 const port = process.env.PORT || 7000
 
-app.use('/api/get-user',profileController)
-app.use('/api/create-user/',userController)
-app.use('/api/follow',followController)
-app.use('/api/create-post/:username',postController)
-app.use('/api/all-posts/:usernameA',followingPostController)
-// app.use('/', flatController)
-// app.use('/signup',register)
-// app.use('/signin', login)
-
+app.use('/follow',followController)
+app.use('/create-post',createPostController)
+app.use('/get-posts',getPostsController )
+app.use('/all-posts/:usernameA',followingPostController)
+app.use('/profile',profileController)
+app.use('/signup',signup)
+app.use('/signin', signin)
+app.use('/google',googleSignIncontroller )
+app.use('/search',searchController)
 
 module.exports = ()=>{
     app.listen(port, async ()=>{
