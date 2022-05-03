74f3a479ae823555d64ecbc36fdcda8601c10962
Vijendra <docs.vijendra@gmail.com>
2022-05-03 10:14:11 +0530

    add joined fileld in user model

--- src/models/user.model.js
+++ src/models/user.model.js
@@ -1,13 +1,31 @@
+const { hashSync, compareSync } = require('bcryptjs')
+const uuid = require('uuid')
 const mongoose = require('mongoose')
+
 const userSchema = new mongoose.Schema(
     {
+        name : { type : String, required : true},
         email : { type : String, required : true},
         password : { type : String, required : true},
-        username : { type : String, default : Date.now().toString()+Math.random()*100000}
+        username : { type : String, default : uuid.v4()},
+        joined : { type : String, required : false},
+        avatar : { type : String, required : false}
     },
     {
         versionKey : false
     }
 )
+
+
+userSchema.pre('save', function(next){
+    if(!this.isModified('password')) 
+        next()
+    this.password = hashSync(this.password)
+    next()
+})
+
+userSchema.methods.checkPassword = function(password){
+    return compareSync(password, this.password)
+}
 
 module.exports = mongoose.model('user', userSchema)
