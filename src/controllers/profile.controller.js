c48b5f17979b739f1e20ecebb0dd1a9bdf2d44f5
Vijendra <docs.vijendra@gmail.com>
2022-05-04 11:51:43 +0530

    removed console.logs and perfomed docs formating.

--- src/controllers/profile.controller.js
+++ src/controllers/profile.controller.js
@@ -6,49 +6,87 @@ const Post = require('../models/post.modal')
 const Follow = require('../models/follow.model')
 const monthArr = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"]
 
-router.get('/:username', async(req, res)=>{
+router.get('/:username/:usernameB', async (req, res) => {
     try {
+        const usernameB = req.params.usernameB
         const username = req.params.username
-        const findUser = await User.findOne({ username}).lean().exec()
-        if(!findUser)   
-            return res
-            .status(404)
-            .send({})
-
         const followers = []
         const following = []
         const posts = []
 
-        const { name, joined, avatar} = await User.findOne({ username : username}).lean().exec()
+
+        const findUser = await User
+            .findOne({ username: usernameB })
+            .lean()
+            .exec()
+        if (!findUser)
+            return res
+                .status(404)
+                .send({})
+
+
+        const { name, joined, avatar } = await User
+            .findOne({ username: usernameB })
+            .lean()
+            .exec()
         const MMDDYYYY = joined.split('/')
-        const MMYYYY = [monthArr[MMDDYYYY[0]-1],MMDDYYYY[2]].join(" ")
-        const followersQuery = await Follow.find({ celeb : username}).lean().exec()
-        followersQuery?.map(obj =>{
+        const MMYYYY = [monthArr[MMDDYYYY[0] - 1], MMDDYYYY[2]].join(" ")
+
+
+        const followersQuery = await Follow
+            .find({ celeb: usernameB })
+            .lean()
+            .exec()
+        followersQuery?.map(obj => {
             followers.push(obj.user)
         })
-        
-        const followingQuery = await Follow.find({ user : username}).lean().exec()
-        if(Array.isArray(followingQuery)){
-            for(let i = 0;i<followingQuery.length;i++){
+
+
+        const isFollwing = await Follow
+            .findOne({ $and: [{ celeb: usernameB }, { user: username }] })
+            .lean()
+            .exec()
+        const status = isFollwing == null || (Array.isArray(isFollwing) && isFollwing.length == 0) ? 0 : 1
+
+
+        const followingQuery = await Follow
+            .find({ user: usernameB })
+            .lean()
+            .exec()
+        if (Array.isArray(followingQuery))
+            for (let i = 0; i < followingQuery.length; i++)
                 following.push(followingQuery[i].celeb)
-            }
-        }
-        else {
+        else
             following.push(followersQuery.celeb)
-        }
-        const postsQuery = await Post.find({ username}).lean().exec()
-        postsQuery?.map(post=>{
+
+
+        const postsQuery = await Post
+            .find({ username: usernameB })
+            .lean()
+            .exec()
+        postsQuery?.map(post => {
             posts.push(post)
         })
+
+
         return res
-        .status(200)
-        .send({name,username,avatar,joined : MMYYYY,followers : followers.length,following: following.length ,posts})
+            .status(200)
+            .send({
+                name,
+                username: usernameB,
+                avatar,
+                joined: MMYYYY,
+                followers: followers.length,
+                following: following.length,
+                posts,
+                status
+            })
     } catch (error) {
         return res
-        .status(500)
-        .send({
-            message : error.message,
-        })
+            .status(500)
+            .send({
+                message: error.message,
+            })
     }
 })
 
