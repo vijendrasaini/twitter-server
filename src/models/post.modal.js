4e3292ed3522201df5614cbe06bebb25f7183c3b
Vijendra <docs.vijendra@gmail.com>
2022-05-04 18:39:57 +0530

    modified likes field as array.

--- src/models/post.modal.js
+++ src/models/post.modal.js
@@ -1,12 +1,12 @@
 const mongoose = require('mongoose')
 const postSchema = new mongoose.Schema(
     {
-            username : { type : String , required : true},
-            avatar : { type : String , required : true}, 
-            name : { type : String , required : true},
-            title : { type : String },
-            image : { type : String },
-            likes : [{type : String}]
+        username : { type : String , required : true},
+        avatar : { type : String , required : true}, 
+        name : { type : String , required : true},
+        title : { type : String },
+        image : { type : String },
+        likes : [{type : String}]
     },
     {
         versionKey : false,
