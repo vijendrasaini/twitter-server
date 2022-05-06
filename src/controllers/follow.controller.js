01de4b1fa72dce82398d9eb0afddaa9413d5042e
Vijendra <docs.vijendra@gmail.com>
2022-05-06 14:00:41 +0530

    comments are removed.

--- src/controllers/follow.controller.js
+++ src/controllers/follow.controller.js
@@ -3,44 +3,45 @@ const User = require('../models/user.model')
 const Follow = require('../models/follow.model')
 const router = express.Router()
 
-router.post('/:username/:usernameB', async ( req, res)=>{
+router.post('/:username/:usernameB', async (req, res) => {
     try {
-            const status = await Follow.create({
-                celeb : req.params.usernameB,
-                user : req.params.username
-            })
+        const status = await Follow.create({
+            celeb: req.params.usernameB,
+            user: req.params.username
+        })
         return res
-        .status(202)
-        .send({  status: "success" })
+            .status(202)
+            .send({ status: "success" })
     } catch (error) {
         return res
-        .status(500)
-        .send({
-            message : error.message
-        })
+            .status(500)
+            .send({
+                message: error.message
+            })
     }
 })
 
-router.delete('/:username/:usernameB', async ( req, res)=>{
+router.delete('/:username/:usernameB', async (req, res) => {
     try {
-            const status = await Follow
-                                .findOneAndDelete(
-                                    { 
-                                        celeb : req.params.usernameB,
-                                        user : req.params.username
-                                    }
-                                )
-                                .lean()
-                                .exec()
+        const status = await Follow
+            .findOneAndDelete(
+                {
+                    celeb: req.params.usernameB,
+                    user: req.params.username
+                }
+            )
+            .lean()
+            .exec()
         return res
-        .status(202)
-        .send({  status: "success" })
+            .status(202)
+            .send({ status: "success" })
     } catch (error) {
         return res
-        .status(500)
-        .send({
-            message : error.message
-        })
+            .status(500)
+            .send({
+                message: error.message,
+                status : "failure"
+            })
     }
 })
 
