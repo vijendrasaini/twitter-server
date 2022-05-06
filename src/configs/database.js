1b4a7fdf6eb97fff25d322e9e8258bf7611951c6
Vijendra <docs.vijendra@gmail.com>
2022-05-06 14:18:40 +0530

    added remote mongo atlas link

--- src/configs/database.js
+++ src/configs/database.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose')
-const mongoDB = "mongodb://localhost:27017/twitter"
-// const mongoDB = `mongodb+srv://vijendra:vijendra@cluster0.mpexw.mongodb.net/apartmentmanagement?retryWrites=true&w=majority`
+
+// const mongoDB = "mongodb://localhost:27017/twitter"
+const mongoDB = "mongodb+srv://twitter:vijendra@cluster0.jsoju.mongodb.net/twitterRemote?retryWrites=true&w=majority"
 
 module.exports = ()=>mongoose.connect(mongoDB)
